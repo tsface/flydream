b6a41ad645fdda1fad81fbca298aba7d5064bc2c
yanli <hanily@msn.com>
2015-05-31 22:08:41 +0800

    [flydream][commit][commit test case of Constant ]

---
+++ test/com/flydream/common/ConstantTestCase.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)Constant.java 2015/5/31
+ */
+package com.flydream.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test for Constant
+ * @author liyan
+ * @version 2015/5/31/
+ * @since 1.0
+ * @see
+ */
+public class ConstantTestCase{
+	
+	@Test
+	public void testConstant(){
+		Assert.assertEquals("login", Constant.LOGIN);
+	}
+}
